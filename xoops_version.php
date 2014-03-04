75669dc8c53474591662d20db9cc5fdc7dc53ba1
Tad <tad0616@gmail.com>
2014-03-04 14:28:52 +0800

    修正區塊在過寬的情形下，注音會無法正確排列的情形

--- xoops_version.php
+++ xoops_version.php
@@ -3,7 +3,7 @@ $modversion = array();
 
 //---模組基本資訊---//
 $modversion['name'] = _MI_TADIDIOMS_NAME;
-$modversion['version'] = 1.1;
+$modversion['version'] = 1.2;
 $modversion['description'] = _MI_TADIDIOMS_DESC;
 $modversion['author'] = _MI_TADIDIOMS_AUTHOR;
 $modversion['credits'] = _MI_TADIDIOMS_CREDITS;
@@ -14,7 +14,7 @@ $modversion['image'] = "images/logo_{$xoopsConfig['language']}.png";
 $modversion['dirname'] = basename(dirname(__FILE__));
 
 //---模組狀態資訊---//
-$modversion['release_date'] = '2014/01/10';
+$modversion['release_date'] = '2014/03/04';
 $modversion['module_website_url'] = 'http://tad0616.net/';
 $modversion['module_website_name'] = _MI_TAD_WEB;
 $modversion['module_status'] = 'release';
