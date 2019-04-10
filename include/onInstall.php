c35a5a1da8a7ee765c154fc2bf88b7a63fe235eb
mambax7 <mambax7@gmail.com>
2019-04-10 11:02:49 -0400

    utility
    Signed-off-by: mambax7 <mambax7@gmail.com>

--- include/onInstall.php
+++ include/onInstall.php
@@ -1,23 +1,13 @@
 <?php
+
+use XoopsModules\Tad_idioms\Utility;
+
+include dirname(__DIR__) . '/preloads/autoloader.php';
+
 function xoops_module_install_tad_idioms(&$module)
 {
 
-    tad_idioms_mk_dir(XOOPS_ROOT_PATH . "/uploads/tad_idioms");
+    Utility::mk_dir(XOOPS_ROOT_PATH . "/uploads/tad_idioms");
 
     return true;
-}
-
-//建立目錄
-function tad_idioms_mk_dir($dir = "")
-{
-    //若無目錄名稱秀出警告訊息
-    if (empty($dir)) {
-        return;
-    }
-    //若目錄不存在的話建立目錄
-    if (!is_dir($dir)) {
-        umask(000);
-        //若建立失敗秀出警告訊息
-        mkdir($dir, 0777);
-    }
 }
