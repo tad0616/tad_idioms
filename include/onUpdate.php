54507db47a075f676cb8f4e559d7270268f7df28
Tad <tad0616@gmail.com>
2019-05-10 16:40:07 +0800

    更新說明

--- include/onUpdate.php
+++ include/onUpdate.php
@@ -1,6 +1,9 @@
 <?php
 
 use XoopsModules\Tadtools\Utility;
+if (!class_exists('XoopsModules\Tadtools\Utility')) {
+    require XOOPS_ROOT_PATH . '/modules/tadtools/preloads/autoloader.php';
+}
 
 function xoops_module_update_tad_idioms(&$module, $old_version)
 {
