3d325c44c9f1259517d5ebf3fb6bead2cb727191
Tad <tad0616@gmail.com>
2019-05-14 09:56:02 +0800

    修正頁面無法正常顯示問題

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ function list_tad_idioms($show_sn = '')
     $andkeyword = '';
     if (isset($_POST['keyword'])) {
         $myts = \MyTextSanitizer::getInstance();
-        $keyword    = $myts->addSlashes($_POST['keyword']);
+        $keyword = $myts->addSlashes($_POST['keyword']);
         $andkeyword = " where `title` like '%{$keyword}%' or `mean` like '%{$keyword}%'";
     }
 
@@ -22,14 +22,14 @@ function list_tad_idioms($show_sn = '')
 
     //Utility::getPageBar($原sql語法, 每頁顯示幾筆資料, 最多顯示幾個頁數選項);
     $PageBar = Utility::getPageBar($sql, 20, 10);
-    $bar     = $PageBar['bar'];
-    $sql     = $PageBar['sql'];
-    $total   = $PageBar['total'];
+    $bar = $PageBar['bar'];
+    $sql = $PageBar['sql'];
+    $total = $PageBar['total'];
 
     $result = $xoopsDB->query($sql) or Utility::web_error($sql, __FILE__, __LINE__);
 
     $all_content = [];
-    $i           = 0;
+    $i = 0;
 
     while (false !== ($all = $xoopsDB->fetchArray($result))) {
         //以下會產生這些變數： $sn , $title , $juin , $mean , $show_times , $search_times , $cate
@@ -41,16 +41,16 @@ function list_tad_idioms($show_sn = '')
         $main = '';
         $sound = [_MD_TADIDIOMS_2, _MD_TADIDIOMS_3, _MD_TADIDIOMS_4, _MD_TADIDIOMS_5];
         foreach ($ji as $n => $juin) {
-            $sud = mb_substr($juin, -2);
+            $sud = substr($juin, -2);
             if (in_array($sud, $sound)) {
-                $juin = mb_substr($juin, 0, -2);
-                $lh = mb_strlen($juin) > 6 ? 80 : 120;
+                $juin = substr($juin, 0, -2);
+                $lh = strlen($juin) > 6 ? 80 : 120;
             } else {
                 $sud = '&nbsp;&nbsp;';
-                $lh = mb_strlen($juin) > 6 ? 80 : 120;
+                $lh = strlen($juin) > 6 ? 80 : 120;
             }
-            $m   = $n * 3;
-            $txt = mb_substr($title, $m, 3);
+            $m = $n * 3;
+            $txt = substr($title, $m, 3);
             $main .= "
               <td style='font-size:2em;font-family:" . _MD_TADIDIOMS_FONT . ";'>$txt</td>
               <td style='font-size:11px;font-family:" . _MD_TADIDIOMS_FONT . ";width:12px;line-height:{$lh}%;'>$juin</td>
@@ -61,11 +61,11 @@ function list_tad_idioms($show_sn = '')
         $newsn = sprintf('%1$03d', $sn);
 
         $all_content[$i]['current'] = $sn == $show_sn ? 1 : 0;
-        $all_content[$i]['name']    = $sn;
-        $all_content[$i]['sn']      = $newsn;
-        $all_content[$i]['main']    = $main;
-        $all_content[$i]['mean']    = $mean;
-        $all_content[$i]['title']   = $title;
+        $all_content[$i]['name'] = $sn;
+        $all_content[$i]['sn'] = $newsn;
+        $all_content[$i]['main'] = $main;
+        $all_content[$i]['mean'] = $mean;
+        $all_content[$i]['title'] = $title;
         ++$i;
     }
 
@@ -75,11 +75,11 @@ function list_tad_idioms($show_sn = '')
 
 /*-----------執行動作判斷區----------*/
 require_once $GLOBALS['xoops']->path('/modules/system/include/functions.php');
-$op      = system_CleanVars($_REQUEST, 'op', '', 'string');
-$sn      = system_CleanVars($_REQUEST, 'sn', 0, 'int');
+$op = system_CleanVars($_REQUEST, 'op', '', 'string');
+$sn = system_CleanVars($_REQUEST, 'sn', 0, 'int');
 $show_sn = system_CleanVars($_REQUEST, 'show_sn', 0, 'int');
 
-$xoopsTpl->assign('toolbar', toolbar_bootstrap($interface_menu));
+$xoopsTpl->assign('toolbar', Utility::toolbar_bootstrap($interface_menu));
 $xoopsTpl->assign('isAdmin', $isAdmin);
 
 switch ($op) {
